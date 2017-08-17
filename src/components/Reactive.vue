942535333893cf70b87661cba2cd68e461852b9c
stepanowon <stepanowon@hotmail.com>
2017-08-17 09:46:00 +0900

    Vue Wrapper for Chart.js

---
+++ src/components/Reactive.vue
@@ -0,0 +1,36 @@
+<script>
+  import {Bar, mixins} from 'vue-chartjs'
+  const { reactiveProp } = mixins
+  export default Bar.extend({
+    mixins: [reactiveProp],
+    data () {
+      return {
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              }
+            }]
+          },
+          legend: {
+            display: true
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
+    }
+  })
+</script>
